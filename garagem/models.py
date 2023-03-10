2fa9b2701ef35d8222d440831a824406a79503f4
viniihds <viniciushsilva2210@gmail.com>
2023-03-10 16:36:19 -0300

    criação model categoria

--- garagem/models.py
+++ garagem/models.py
@@ -9,3 +9,8 @@ class Marca(models.Model):
     def __str__(self):
         return self.nome.upper()
 
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao

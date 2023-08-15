9b16943cbf7e14d6eaf356b423dc0fb6574ac547
viniihds <viniciushsilva2210@gmail.com>
2023-08-15 14:06:00 -0300

    Models divididas em diferentes arquivos

---
+++ garagem/models/__init__.py
@@ -0,0 +1,5 @@
+from .categoria import Categoria
+from .acessorio import Acessorio
+from .cor import Cor
+from .marca import Marca
+from .veiculo import veiculo
---
+++ garagem/models/veiculo.py
@@ -0,0 +1,17 @@
+from django.db import models
+from .marca import Marca
+from .cor import Cor
+from .categoria import Categoria
+
+class veiculo(models.Model):
+    modelo = models.CharField(max_length=150, default= "")
+    ano = models.IntegerField(null= True, default=0)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, null= True, default=0)
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT, related_name="veiculos")
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculos")
+
+    def __str__(self):
+        return f"{self.modelo}({self.cor}, {self.marca}, {self.ano})"
+    class Meta:
+        verbose_name = "veículo"

6caed4d00c498264374dd37345f444b53433b110
viniihds <viniciushsilva2210@gmail.com>
2023-08-15 15:07:49 -0300

    Correção Model Veiculo

--- garagem/models/veiculo.py
+++ garagem/models/veiculo.py
@@ -1,14 +1,16 @@
 from django.db import models
 from .modelo import Modelo
 from .cor import Cor
+from .acessorio import Acessorio
 
 
 class Veiculo(models.Model):
     modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculos")
     ano = models.IntegerField(null= True, default=0)
     preco = models.DecimalField(max_digits=10, decimal_places=2, null= True, default=0)
-
+    descricao = models.CharField(max_length=500, default="")
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
+    acessorio = models.ManyToManyField(Acessorio, related_name="veiculos")
 
     def __str__(self):
         return f"{self.modelo}({self.cor}, {self.ano})"
